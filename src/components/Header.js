e12a3783615a9b8b6e6ff7799b2eabb5b807b5fb
Ismael Moreno <ismaelmoreno2@hotmail.es>
2023-02-10 15:23:46 -0800

    fixed problem with resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -89,7 +89,7 @@ class Header extends Component {
               />
             </div>
           </div>
-              <a class="btn btn-primary btn-lg" target="_blank" href="IsmaelMoreno.pdf"><h2>My Resume</h2></a>
+              <a class="btn btn-primary btn-lg" href="IsmaelMoreno.pdf"><h2>My Resume</h2></a>
         </div>
       </header>
     );
